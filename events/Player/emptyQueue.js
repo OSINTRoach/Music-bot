ec6ff764e0cac1f3ccc0d552ede92209c09ca463
David Buday <david.buday@gauss.dev>
2023-11-30 21:55:24 +0100

    reset activity on empty queue

--- events/Player/emptyQueue.js
+++ events/Player/emptyQueue.js
@@ -4,5 +4,6 @@ module.exports = (queue) => {
     .setAuthor({name: `No more songs in the queue! ❌`})
     .setColor('#2f3136')
 
+    global.client.user.setActivity(client.config.app.playing)
     queue.metadata.send({ embeds: [emptyQueue] })
 }
