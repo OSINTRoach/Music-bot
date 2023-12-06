6b61462c2b79e8e3b812d0e04b104eda1d1d933b
David Buday <david.buday@gauss.dev>
2023-12-06 20:48:40 +0100

    josip

---
+++ commands/core/josip.js
@@ -0,0 +1,12 @@
+const ms = require('ms');
+
+module.exports = {
+    name: 'Josip',
+    description: "Šta je josip",
+    async execute({ client, inter }) {
+
+        // const m = await inter.editReply("Test?")
+        inter.editReply(`Noob`)
+
+    },
+};
